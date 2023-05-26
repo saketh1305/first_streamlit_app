21a18413167f11c94c5766543ba69785a050506b
Saketh Reddy Regatte <sakethreddy1305@gmail.com>
2023-05-26 00:33:11 -0700

    Added breafast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, spinach  rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
