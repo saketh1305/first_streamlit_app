b20b4c17c25a60f3c7687bf924f9df01fba00d6f
Saketh Reddy Regatte <sakethreddy1305@gmail.com>
2023-05-26 00:35:16 -0700

    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('BreakFast Menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, spinach  rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text('🥗 Kale, spinach  rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
+
+  
